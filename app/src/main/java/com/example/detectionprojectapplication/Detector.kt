c0f3562a6bfdd6b725f2906af7633023836bc3f3
JelAnt <ante.jelavi12@gmail.com>
2025-03-09 19:57:46 +0000

    Initialize Android Studio Project

---
+++ app/src/main/java/com/example/detectionprojectapplication/Detector.kt
@@ -0,0 +1,20 @@
+package com.example.detectionprojectapplication
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.SystemClock
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class Detector {
+}
